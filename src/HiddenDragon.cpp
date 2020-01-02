ae161c9cd15a63630ce81ae63eac5bf4f7e4514c
petter.hansson <me@petterhansson.se>
2020-01-02 02:41:47 +0100

    Decided to use the real DirectPlay (IID_IDirectPlay4A) since Close Combat version was older than expected.

--- src/HiddenDragon.cpp
+++ src/HiddenDragon.cpp
@@ -4,18 +4,75 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
+#include <dplay.h>
+#include <dpaddr.h>
+#include <dxerr8.h>
+
+
+IDirectPlay* _baseDirectPlay = nullptr;
+IDirectPlay4A* _directPlay = nullptr;
+
+
+static BOOL FAR PASCAL directPlayEnumHandler(LPCDPSESSIONDESC2   lpThisSD,
+	LPDWORD             lpdwTimeOut,
+	DWORD               dwFlags,
+	LPVOID              lpContext)
 {
-    std::cout << "Hello World!\n"; 
+	if (DPESC_TIMEDOUT == dwFlags || lpThisSD == nullptr)
+	{
+		return false;
+	}
+
+	std::cout << "Found session " << lpThisSD->lpszSessionNameA << std::endl;
+	return false;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void onExit()
+{
+	_directPlay->Release();
+	_baseDirectPlay->Release();
+	CoUninitialize();
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+	atexit(onExit);
+	CoInitializeEx(NULL, COINIT_MULTITHREADED);
+
+    std::cout << "Welcome to Hidden Dragon, a bot for Close Combat 3: Cross of Iron\n";
+
+	//{78d77218-76e4-cb41-8c06-7dce98bd2cb7}
+	//{1872D778-E476-41CB-8C06-7DCE98BD2CB7}
+
+	GUID appGuid;
+	const HRESULT guidResult = CLSIDFromString(L"{1872D778-E476-41CB-8C06-7DCE98BD2CB7}", (LPCLSID)&appGuid);
+	if (guidResult != S_OK)
+	{
+		std::cerr << "Invalid DirectPlay application GUID\n";
+		return 1;
+	}
+
+	if (FAILED(DirectPlayCreate((LPGUID)&DPSPGUID_TCPIP, &_baseDirectPlay, nullptr)))
+	{
+		std::cerr << "Failure to get base DirectPlay interface\n";
+		return 1;
+	}
+
+	if (FAILED(_baseDirectPlay->QueryInterface(IID_IDirectPlay4A, (LPVOID*)&_directPlay)))
+	{
+		std::cerr << "Failure to query DirectPlay 4 interface\n";
+		return 1;
+	}
+
+	DPSESSIONDESC2 sessionDesc;
+	std::memset(&sessionDesc, 0, sizeof(sessionDesc));
+	sessionDesc.dwSize = sizeof(sessionDesc);
+	sessionDesc.guidApplication = appGuid;
+	if (FAILED(_directPlay->EnumSessions(&sessionDesc, 0, directPlayEnumHandler, nullptr, DPENUMSESSIONS_ALL)))
+	{
+		std::cerr << "Failure to enumerate DirectPlay sessions\n";
+		return 1;
+	}
+
+	std::cout << "Successfully enumerated hosts\n";
+}
